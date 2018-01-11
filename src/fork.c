7c7d51a5cf7668b09492597b35f64939df447102
Carsten Michel <carsten.michel@de.ibm.com>
2018-01-11 10:14:02 +0100

    correct usage of config.h

--- src/fork.c
+++ src/fork.c
@@ -1,42 +1,66 @@
+#include "config.h"
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
-#include <sys/time.h>
 
-int main() {
-    FILE* fd;
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+
+void checkIfFunctionsDoExist()
+{
+#ifndef HAVE_GETTIMEOFDAY
+    printf("getTimeOfDay not supported");
+    exit(1);
+#endif
+#ifndef HAVE_STRERROR
+    printf("strerr not supported");
+    exit(2);
+#endif
+}
+
+int main()
+{
+    FILE *fd;
     int charactersWritten = 0;
     int returnCode = 0xff;
-    struct timeval  first, second;
+    struct timeval first, second;
+    checkIfFunctionsDoExist();
 
-    
     fd = fopen("test.txt", "w");
-    charactersWritten =  fprintf(fd, "Hello World!\n");
-
+    charactersWritten = fprintf(fd, "Hello World!\n");
 
     gettimeofday(&first, NULL);
     returnCode = fclose(fd);
     gettimeofday(&second, NULL);
     printf("First close took %d \n", second.tv_usec - first.tv_usec);
-    if (returnCode ==0) {
+    if (returnCode == 0)
+    {
         printf("First close was successfull\n");
-    } else {
+    }
+    else
+    {
         printf("First close was *NOT* ok \n");
-    } 
-    
+    }
+
     gettimeofday(&first, NULL);
     returnCode = fclose(fd);
     gettimeofday(&second, NULL);
     printf("Second close took %d \n", second.tv_usec - first.tv_usec);
-    if (returnCode ==0) {
+    if (returnCode == 0)
+    {
         printf("Second close was successfull\n");
-    } else {
+    }
+    else
+    {
         printf("Second close was *NOT* ok with %d \n", returnCode);
         printf("Errno is %d\n", errno);
-        char* text = strerror((int)errno);
+        char *text = strerror((int)errno);
         printf("Error string is %s \n", text);
-    } 
-
+    }
 
     return 0;
 }
